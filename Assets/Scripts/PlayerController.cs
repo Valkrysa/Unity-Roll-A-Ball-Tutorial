66afb858d669178497b9d0bd76cf6d8227deb5ac
Alice Robinson <valkrysa@gmail.com>
2017-01-10 22:20:24 -0800

    copied over project to get it into version control

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+  private Rigidbody rb;
+  private int count;
+
+  public float speed;
+  public Text countText;
+  public Text winText;
+  public AudioSource audio;
+  public AudioClip coin_audio;
+  public AudioClip win_audio;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+    count = 0;
+    SetCountText();
+    winText.text = "";
+    audio = GetComponent<AudioSource>();
+	}
+
+	void FixedUpdate() {
+		// CTRL + ' opens documentation while selecting a word
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVeritcal = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVeritcal);
+
+    rb.AddForce(movement * speed);
+	}
+
+  void OnTriggerEnter(Collider other){
+    if(other.gameObject.CompareTag("Pick Up")){
+      other.gameObject.SetActive(false);
+      count = count + 1;
+      SetCountText();
+      audio.PlayOneShot(coin_audio);
+      if(count >= 13) {
+        winText.text = "You Win!";
+        audio.PlayOneShot(win_audio);
+      }
+    }
+  }
+
+  void SetCountText(){
+    countText.text = "Count: " + count.ToString();
+  }
+		
+}
